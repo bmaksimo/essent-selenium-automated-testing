070c542f366c2d05cdb5f3548fad9c8cd0753f28
Fernando Mano <fernando.mano@billinghouse.nl>
2018-06-18 16:16:20 -0300

    NSTA-121: get column index list

--- src/test/resources/js/runner/tests/TrGetColumnIndexList.js
+++ src/test/resources/js/runner/tests/TrGetColumnIndexList.js
@@ -15,30 +15,32 @@ class TrGetColumnIndexList extends TestRunnerBase {
     }
 
     run() {
-        let result = this.result;
-        let options = this.options;
-        result.status = 'FAILED';
-        result.reason = 'Not executed';
-        result.column = {"index" : -1, "caption": options.column};
-        let index = $(".list__content th:contains('" + options.column + "')").index();
-        if (index < 0) {
+        setTimeout(()=> {
+            let result = this.result;
+            let options = this.options;
             result.status = 'FAILED';
-            result.reason = 'Column ' + options.column + ' was not found.';
-            console.log("index: " + index);
-        } else if ($("#rows tr:nth-child(1)") < 0) {
-            result.status = 'FAILED';
-            result.reason = 'Row was not found.';
-            console.log(result);
-        } else {
-            result.status = 'PASSED';
-            result.column.index = ++index;
-            let elem = $("#rows tr:nth-child(1) td:nth-child(" + index + ") div a")
-            result.reason = 'Navigation, click on ' + elem.text();
-            result.column.caption = options.column;
-            console.log(result);
-            elem.click();
-        }
-        this.resolveCallback(result);
+            result.reason = 'Not executed';
+            result.column = {"index": -1, "caption": options.column};
+            let index = $(".list__content th:contains('" + options.column + "')").index();
+            if (index < 0) {
+                result.status = 'FAILED';
+                result.reason = 'Column ' + options.column + ' was not found.';
+                console.log("index: " + index);
+            } else if ($("#rows tr:nth-child(1)") < 0) {
+                result.status = 'FAILED';
+                result.reason = 'Row was not found.';
+                console.log(result);
+            } else {
+                result.status = 'PASSED';
+                result.column.index = ++index;
+                let elem = $("#rows tr:nth-child(1) td:nth-child(" + index + ") div a");
+                result.reason = 'Navigation, click on ' + elem.text();
+                result.column.caption = options.column;
+                console.log(result);
+                elem.click();
+            }
+            this.resolveCallback(result);
+        }, 5000);
     }
 
 }
