af4792708b2a514a07c44413fef126f7672b8bbf
Fernando Mano <fernando.mano@billinghouse.nl>
2018-06-18 18:54:20 -0300

    NSTA-121: organize imports

--- src/test/java/stepdefinitions/dwp/javascript/JavascriptTestRunnerTest.java
+++ src/test/java/stepdefinitions/dwp/javascript/JavascriptTestRunnerTest.java
@@ -1,7 +1,6 @@
 package stepdefinitions.dwp.javascript;
 
 import com.billinghouse.javascript.model.Data;
-import com.billinghouse.javascript.testrunner.dwp.menu.MenuTests;
 import com.essent.testing.dwp.DwpScenario;
 import com.essent.testing.dwp.menu.model.DwpLeftMenu;
 import com.essent.testing.dwp.menu.model.TopMenuItems;
