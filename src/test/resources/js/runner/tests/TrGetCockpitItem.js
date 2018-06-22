375a9572aea4084002bbc26118b66d250b5e6b66
LAPTOP-77IPFO76\chris <chris.pasti@billinghouse.nl>
2018-06-22 15:17:41 +0200

    WIP Cockpit Item clickable

---
+++ src/test/resources/js/runner/tests/TrGetCockpitItem.js
@@ -0,0 +1,27 @@
+class TrGetCockpitItem extends TestRunnerBase {
+
+    constructor(options, callback) {
+        super(options, callback, 100);
+    }
+
+    run() {
+        let result = this.result;
+        const options = this.options;
+        result.status = 'UNDEFINED';
+        result.reason = 'Not executed';
+        let matches = [];
+        $("dashboard-item[label='"+options.item+"']");
+)
+        if(matches.length > 0) {
+            matches[0].find('a').trigger('click');
+            result.status = 'PASSED';
+            result.reason = '';
+        } else {
+            result.status = 'FAILED';
+            result.reason = 'Element ' + options.label + ' not found';
+        }
+        setTimeout(()=> {
+            this.resolveCallback(result);
+        }, 5000);
+    }
+}
