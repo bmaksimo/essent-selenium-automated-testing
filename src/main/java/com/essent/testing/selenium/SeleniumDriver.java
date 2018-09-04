688c40b58d2f223f54d720d00b06bb807f8e2f5a
sonja <s.novakovic@levi9.com>
2018-09-04 15:43:14 +0200

    NSTA-176 removed comment from driver.quit()

--- src/main/java/com/essent/testing/selenium/SeleniumDriver.java
+++ src/main/java/com/essent/testing/selenium/SeleniumDriver.java
@@ -182,8 +182,8 @@ public class SeleniumDriver implements JavascriptExecutor, JavascriptTestRunner 
     }
 
     public void tearDown() {
-        //driver.close();
-        //driver.quit();
+        driver.close();
+        driver.quit();
     }
 
     private void injectJavaScriptInline(File functionFile) {
