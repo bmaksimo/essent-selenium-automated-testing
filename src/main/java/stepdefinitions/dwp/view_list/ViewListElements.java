a571940d5f1d4527313b7ed677e978a4a9380349
sonja <s.novakovic@levi9.com>
2018-09-07 15:18:05 +0200

    NSTA-176 resolving conflicts

--- src/main/java/stepdefinitions/dwp/view_list/ViewListElements.java
+++ src/main/java/stepdefinitions/dwp/view_list/ViewListElements.java
@@ -226,7 +226,7 @@ public class ViewListElements extends NavigationElements {
         }
     }
 
-    @Before("@SMOKE, @QUOTE, @QUOTE_MI, @QUOTE_SS, @B2B_REGRESSION")
+   @Before("@SMOKE, @QUOTE, @QUOTE_MI, @QUOTE_SS, @B2B_REGRESSION")
     public void setupTest(Scenario scenario) throws Throwable {
         registerActiveScenario(scenario);
     }
