770e95b22c56001188270896b3ece24a1cff58a4
LAPTOP-77IPFO76\chris <chris.pasti@billinghouse.nl>
2018-09-18 13:06:16 +0200

    Added left menu for Odoo

--- src/featurefiles/smoke_tests/coda/import_coda.feature
+++ src/featurefiles/smoke_tests/coda/import_coda.feature
@@ -1,9 +1,10 @@
 @ODOO
 @CODA
-Feature: Create a coda file
+Feature: Import a coda file
 
     Background:
         Given I logged in to Odoo as payments.testautomation
 
     Scenario: Create a CODA file
         When Odoo top menu is Accounting
+        And Odoo left menu is Import CODA Files
--- src/main/java/stepdefinitions/odoo/menu/OdooTopMenu.java
+++ src/main/java/stepdefinitions/odoo/menu/OdooTopMenu.java
@@ -1,10 +1,20 @@
 package stepdefinitions.odoo.menu;
 
 import com.essent.testing.odoo.scenario.OdooScenario;
+import com.essent.testing.selenium.SeleniumDriver;
 import cucumber.api.Scenario;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.ElementNotVisibleException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import stepdefinitions.odoo.navigation.OdooNavigationElements;
+
+import java.time.Duration;
 
 public class OdooTopMenu extends OdooNavigationElements {
 
@@ -15,6 +25,9 @@ public class OdooTopMenu extends OdooNavigationElements {
 
     @When("^Odoo top menu is ([^\"]*)$")
     public void OdooCheckTopMenuAction(String menu) throws Throwable {
-        OdooClickTopMenuAction(menu);
+        By xpath = By.xpath("//*[@id=\"oe_main_menu_placeholder\"]/ul[1]/li[4]/a");
+        WebElement element = webDriver.findElementOrNull(xpath);
+        element.click();
+
     }
 }
