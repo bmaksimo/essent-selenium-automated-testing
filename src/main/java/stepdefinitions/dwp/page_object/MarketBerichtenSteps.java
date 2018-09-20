5d6dd403eb60877ab053e02e5de32edc06524f46
Strahinja Vuckovic <s.vuckovic@levi9.com>
2018-09-20 09:23:52 +0200

    WIP

--- src/main/java/stepdefinitions/dwp/page_object/MarketBerichtenSteps.java
+++ src/main/java/stepdefinitions/dwp/page_object/MarketBerichtenSteps.java
@@ -9,6 +9,7 @@ import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
 
 public class MarketBerichtenSteps extends DwpScenario {
     BaseObject baseObject = new BaseObject(webDriver);
@@ -46,6 +47,7 @@ public class MarketBerichtenSteps extends DwpScenario {
 
     @Then("^Validate rejection$")
     public void validateRejection() throws Throwable {
-
+        final String time = webDriver.findElementWhenVisible(By.xpath("//tbody[@id='rows']/tr[1]/td[6]/list-simple-two-liner-cell[@icon='null']/p/span[1]")).getText();
+        System.out.println("TIME is " + time);
     }
 }
