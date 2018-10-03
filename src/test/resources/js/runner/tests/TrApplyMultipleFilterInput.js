271eefcb0bd5eaa4fe672658e346173d09506ebd
Fernando Mano <fernando.mano@billinghouse.nl>
2018-10-03 14:36:19 -0300

    NSTA-198: preparing PR

--- src/test/resources/js/runner/tests/TrApplyMultipleFilterInput.js
+++ src/test/resources/js/runner/tests/TrApplyMultipleFilterInput.js
@@ -9,32 +9,22 @@ class TrApplyMultipleFilterInput extends TestRunnerBase {
         result.status = 'UNDEFINED';
         result.reason = 'Not executed';
         const options = this.options;
-            let searchInput = $('#search-input');
+        let searchInput = $('#search-input');
         const searchButton = $('input[type="submit"]');
         if (searchInput.length === 0 || searchButton.length === 0) {
             result.status = 'FAILED';
-            result.reason = 'Filter not available.';
+            result.reason = 'Product filter not available.';
         } else {
-            console.log("options.searchInput");
-            console.log(options.searchInput);
-
-            // setTimeout(function() {
-                searchInput.val(options.searchInput).trigger('change');
-                // searchInput.val("\""+options.searchInput+"\"");
-                console.log("new val: " + searchInput.val());
-            // }, 5000);
-
-            setTimeout(function() {
-                searchButton.trigger('click');
-                }, 5000);
-            let searchInputTerm = $('.multi-select__results input[type=checkbox]');
-            if (searchInputTerm === 0 || searchInputTerm.length === 0) {
+            searchInput.val(options.product);
+            searchButton.click();
+            let product = $('.multi-select__results input[type=checkbox]');
+            if (product === 0 || product.length === 0) {
                 result.status = 'FAILED';
-                result.reason = 'Filter not available.';
+                result.reason = 'Product filter not available.';
             } else {
-                setTimeout(function() { searchInputTerm.prop('checked', true); }, 5000);
-                const submitModal = $('.modal__header > [class=button]');
-                submitModal.trigger('click');
+                product.prop('checked', true);
+                const submitModal = $('.modal__header > .button');
+                submitModal.click();
                 result.status = 'PASSED';
                 result.reason = '';
             }
