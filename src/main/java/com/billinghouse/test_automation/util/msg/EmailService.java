b0a51f9641c6341479c49022d41101eb6b30450e
Fernando Mano <fernando.mano@billinghouse.nl>
2018-11-22 15:49:32 +0100

    NSTA-274: remove html generation

--- src/main/java/com/billinghouse/test_automation/util/msg/EmailService.java
+++ src/main/java/com/billinghouse/test_automation/util/msg/EmailService.java
@@ -4,7 +4,6 @@ import com.essent.testing.config.ConfigKey;
 import com.essent.testing.config.ConfigProvider;
 import com.essent.testing.util.resource.ResourceUtil;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.text.StrSubstitutor;
 import org.apache.log4j.Logger;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -13,13 +12,9 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class EmailService {
@@ -31,13 +26,10 @@ public class EmailService {
     private static final Logger logger = Logger.getLogger(EmailService.class);
 
     private static final String PATH = "/email/";
-    private static final String OUTPUT = "/html/";
     private static final String NUAT5019_TEMPLATE = "nuat5019_template.tpl";
-    private static final String NUAT5019_result = "nuat-5019-result.html";
 
     public boolean createTestReport(Map input) {
         String messageBody = getMessageFromTemplate(input);
-        if (StringUtils.isNotBlank(messageBody)) writeToFile(messageBody);
         return sendEmailNow(getJavaMailSender(), messageBody);
     }
 
@@ -64,15 +56,6 @@ public class EmailService {
         }
 
         return null;
-    }
-
-    private void writeToFile(String content) {
-        try {
-            Files.write(Paths.get(ResourceUtil.toPath(OUTPUT + NUAT5019_result)), content.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        logger.info("Done writing file to " + OUTPUT + NUAT5019_result);
     }
 
     private boolean sendEmailNow(JavaMailSender emailSender, String messageBody) {
