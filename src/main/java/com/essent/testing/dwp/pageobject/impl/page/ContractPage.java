2eddc2ccdcaaef635698188bd7dfd75abda883ea
nikola_g <n.gajic@levi9.com>
2018-12-11 15:29:36 +0100

    added new scripts and feature file for TK1 contract creation
    Cherry picked from branch Active-Contract-TK1-NUAT-412 to fix case of folder "jbilling" that mysteriously disappeared

--- src/main/java/com/essent/testing/dwp/pageobject/impl/page/ContractPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/impl/page/ContractPage.java
@@ -57,4 +57,72 @@ public class ContractPage extends Component {
         return seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"rows\"]/tr[1]/td[2]/list-simple-two-liner-cell/p/span[1]")).getText();
     }
 
+    public void clickOnX() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//guidance-modal//div[@class = 'modal__header']/a")));
+
+    }
+
+    public void selectItemLegalForm() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"legal-form-c-field\"]/option[2]")));
+    }
+
+    public void selectGender() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"gender-c-field\"]/option[2]")));
+    }
+
+    public void getEmail(String emailContract) {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"leads-contact-details-contact-details-type-email-contact-details-value-field\"]")), emailContract);
+    }
+
+    public void clickNaceCode() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"nace-code-c-field\"]")));
+    }
+
+    public void clickOnSearch() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//div[@class = 'input__with-button']/input")));
+    }
+
+    public void checkNaceCode() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//select-with-search-modal/section//span")));
+    }
+
+    public void saveSelectedItem() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//select-with-search-modal/section//div[@class = 'modal__header']/a")));
+    }
+
+    public void setAddress(String address, String houseNumber, String postalCode, String City) {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"address-street-field\"]")), address);
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"address-number-field\"]")), houseNumber);
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"address-postalcode-field\"]")), postalCode);
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"address-city-field\"]")), City);
+
+    }
+
+    public void setTelephone(String telephone) {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"leads-contact-details-contact-details-phone-type-work-phone-contact-details-type-phone-contact-details-value-field\"]")), telephone);
+    }
+
+    public void setName (String fname, String lname) throws Throwable {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"first-name-field\"]")), fname);
+        Thread.sleep(2000);
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"last-name-field\"]")), lname);
+
+    }
+
+    public void setCompanyName(String cname) {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"company-name-c-field\"]")), cname);
+
+    }
+
+    public void setEanCode(String eanCode) {
+        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"ean-c-accounts-aos-quotes-aos-products-quotes-7-cc-91145-f-43-f-4800-d-705-58-ffa-7899-fbd-field\"]")), eanCode);
+
+    }
+
+    public void saveInitialQuote() {
+        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//*[@id=\"primaryButton\"]/span[2]")));
+
+    }
+
+
 }
--- src/main/java/stepdefinitions/dwp/b2b/Contract.java
+++ src/main/java/stepdefinitions/dwp/b2b/Contract.java
@@ -2,9 +2,11 @@ package stepdefinitions.dwp.b2b;
 
 import com.essent.testing.dwp.pageobject.impl.page.BaseObject;
 import com.essent.testing.dwp.pageobject.impl.page.ContractPage;
+import com.essent.testing.dwp.pageobject.impl.quote.QuoteDetailsPage;
 import com.essent.testing.dwp.pageobject.impl.service_contracting.ContractenPage;
 import com.essent.testing.dwp.scenario.DwpScenario;
 import cucumber.api.Scenario;
+import static org.hamcrest.core.Is.is;
 import cucumber.api.java.Before;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
@@ -12,6 +14,10 @@ import cucumber.api.java.en.When;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.testng.AssertJUnit.assertEquals;
+import stepdefinitions.dwp.tables.SalesChannel;
+import static org.junit.Assert.assertThat;
+
+
 
 @ContextConfiguration("classpath:stepdefinitions/cucumber.xml")
 
@@ -67,4 +73,100 @@ public class Contract extends DwpScenario {
         webDriver.waitForRequestsToFinish();
         assertEquals(cp.status(),status);
     }
+
+    @And("^Clicked on sign X$")
+    public void clickOnX() throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        webDriver.waitForRequestsToFinish();
+        cp.clickOnX();
+    }
+
+
+    @When("^B2B sales channel is ([^\"]*)$")
+    public void initSalesChannelB2B(SalesChannel salesChannel) throws Throwable {
+        QuoteDetailsPage quoteDetailsPage = new QuoteDetailsPage(webDriver);
+        quoteDetailsPage.setSalesChannel(salesChannel);
+        boolean formInitialized = quoteDetailsPage.fillInFormData();
+        assertThat("Failure occurred when filling in input values", formInitialized, is(true));
+    }
+
+
+    @When("^Rechtsvorm is bvba")
+    public void formLegal() throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.selectItemLegalForm();
+    }
+
+    @And("^Geslacht is Male")
+    public void gender() throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.selectGender();
+    }
+
+    @And("^E-mailadres is ([^\"]*)$")
+    public void emailContract(String emailContract) throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.getEmail(emailContract);
+    }
+
+
+    @And("^Select Nace-Code")
+    public void select() throws Throwable {
+        webDriver.waitForRequestsToFinish();
+        Thread.sleep(2000);
+        ContractPage cp = new ContractPage(webDriver);
+        cp.clickNaceCode();
+    }
+
+    @And("^NaceCode in search is ([^\"]*)$")
+    public void searchByNaceCode(String NaceCode) throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.searchByClientNuiber(NaceCode);
+        cp.clickOnSearch();
+        cp.checkNaceCode();
+        cp.saveSelectedItem();
+    }
+
+
+    @And("^Customer Details are populated with: Address is ([^\"]*) and HouseNumber is \"([^\"]*)\" and PostalCode is \"([^\"]*)\" and City is \"([^\"]*)\"$")
+    public void populateAddress(String Address, String houseNumber, String postalCode, String City) throws Throwable {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.setAddress(Address, houseNumber, postalCode, City);
+    }
+
+    @And("^Telefoon is ([^\"]*)$")
+    public void populateTelephone(String telephone) {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.setTelephone(telephone);
+
+    }
+
+    @And("^First Name is ([^\"]*) and Last Name is \"([^\"]*)\"$")
+    public void populateName(String fname, String lname) throws Throwable{
+        ContractPage cp = new ContractPage(webDriver);
+        cp.setName(fname, lname);
+    }
+
+    @And("^BEDRIJFSNAAM is ([^\"]*)$")
+    public void companyName(String cname) {
+        ContractPage cp = new ContractPage(webDriver);
+        cp.setCompanyName(cname);
+    }
+
+
+    @And("^Ean-Code is ([^\"]*)$")
+    public void eanCode(String eancode) {
+        webDriver.waitForRequestsToFinish();
+        ContractPage cp = new ContractPage(webDriver);
+        cp.setEanCode(eancode);
+    }
+
+
+    @And("^New Quote is saved$")
+    public void newQuoteSaved() throws Throwable {
+        ContractPage quoteInitial = new ContractPage(webDriver);
+        quoteInitial.saveInitialQuote();
+    }
+
+
 }
