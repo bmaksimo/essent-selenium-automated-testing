ac6f6e9b1eab072347f5517f39c435e9c1cdde35
Fernando Mano <fernando.mano@billinghouse.nl>
2018-12-14 12:56:12 +0100

    NSTA-191: parameterise script call

--- src/test/resources/js/runner/tests/TrCheckFirstRowByOption.js
+++ src/test/resources/js/runner/tests/TrCheckFirstRowByOption.js
@@ -9,14 +9,14 @@ class TrCheckFirstRowByOption extends TestRunnerBase {
         result.status = 'FAILED';
         result.reason = 'The text ' + options.text + 'cannot be found in the column ' + options.column + ' for the row selected by ' + options.option;
 
-        let list = $("h2:contains('Transacties')").closest("list").get(0)
+        let list = $("h2:contains('" + options.list + "')").closest("list").get(0);
         $(list).find("th").each(function(index) {
-            if($(this).text() == options.column) {
+            if($(this).text() === options.column) {
                 let row = $(list).find("tr:has(td:contains('"+options.option+"'))").get(0);
 
                 // The following line strips out the string ' €' from the content of the td.
                 // TODO Find a way to render the € sign in the .feature file, so this can be removed. At the moment, passing the € sign from the feature file, it returns a question mark (?).
-                if($(row).children('td').eq(index).get(0).textContent.trim().replace(' \u20AC', '') == options.text) {
+                if($(row).children('td').eq(index).get(0).textContent.trim().replace(' \u20AC', '') === options.text) {
                     result.status = 'PASSED';
                     result.reason = '';
                 } else {
