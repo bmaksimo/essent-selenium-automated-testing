8f145bc2cec25db88447ecede73a711a94b26346
mkolarov <mi.kolarov@levi9.com>
2019-01-17 16:10:17 +0100

    added shell for steps

---
+++ src/test/java/stepdefinitions/quote/api/IloginToiWelcomeAs.java
@@ -0,0 +1,106 @@
+package stepdefinitions.quote.api;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+
+public class IloginToiWelcomeAs {
+    @Given("^I login to iWelcome as \"([^\"]*)\"$")
+    public void i_login_to_iWelcome_as(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^\"([^\"]*)\" flow is started$")
+    public void flow_is_started(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Data is prepared for Create qoute request for \"([^\"]*)\"$")
+    public void data_is_prepared_for_Create_qoute_request_for(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^New tc(\\d+)_quote is created$")
+    public void new_tc__quote_is_created(int arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Quote status is \"([^\"]*)\"$")
+    public void quote_status_is(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Quoteline exists$")
+    public void quoteline_exists() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Quoteline status is \"([^\"]*)\"$")
+    public void quoteline_status_is(String arg1) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Simulation that customer signature is recieved$")
+    public void simulation_that_customer_signature_is_recieved() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^File is uploaded as scanned signature$")
+    public void file_is_uploaded_as_scanned_signature() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Signin is confirmed$")
+    public void signin_is_confirmed() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Contract is created$")
+    public void contract_is_created() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Contracted EAN exists on account$")
+    public void contracted_EAN_exists_on_account() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Quote details are recieved$")
+    public void quote_details_are_recieved() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Payment detials are recieved$")
+    public void payment_detials_are_recieved() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Wait until contract instance starts$")
+    public void wait_until_contract_instance_starts() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^Check if end time is valid$")
+    public void check_if_end_time_is_valid() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
