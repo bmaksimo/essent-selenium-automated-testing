2b4b1cb4444b7bf95d9353354d5eea1ab60ce048
Dmitry Chebayewski <dmitry.chebayewski@billinghouse.nl>
2019-02-14 10:08:08 +0100

    WIP working on inventory of Gherkin steps

--- eaudecologne-ekstrakts.sh
+++ eaudecologne-ekstrakts.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-allcukes=allcukes-processed.txt
+ekstrakts=allcukes-ekstrakts.txt
 
-echo 'Pouring Gherkin steps patterns (cologne extract) to '"${allcukes}"
-find src/ -name '*.java' -exec cat {} \; | grep '^\s*\(@Given\|@When\|@Then\|@And\)' | sed -n "s/^[[:blank:]]*@[[:alpha:]]*(/(/p" | sort >> "${allcukes}"
+echo 'Pouring Gherkin steps definitions (cologne extract) to '"${ekstrakts}"
+find src/ -name '*.java' -exec cat {} \; | grep '^\s*\(@Given\|@When\|@Then\|@And\)' | sed -n "s/^[[:blank:]]*@[[:alpha:]]*(/(/p" | sort >> "${ekstrakts}" 
