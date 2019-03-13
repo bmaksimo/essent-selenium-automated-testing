c4500f3f0d274b99cb03622020bce8c5b484890f
Branislava Maksimovic <branislava.maksimovic@gmail.com>
2019-03-13 09:46:38 +0100

    Changed default name for account Added credentials for DEVINT03 Chaned CRM credentials for DEVINT02 CRM and DWP essentadmin user

--- src/main/java/stepdefinitions/quote/api/QuoteDetailsAPI.java
+++ src/main/java/stepdefinitions/quote/api/QuoteDetailsAPI.java
@@ -272,7 +272,7 @@ public class QuoteDetailsAPI extends AbstractAPI {
 
         String firstName = "default";
         String lastName = "default";
-        String accountName = "RA_" + startedFlowName + "_TC1_B2C_123456789012345";
+        String accountName = "RA_" + startedFlowName + "_TC1_B2C_YMR_MoveIn";
         String reversedLastName = "";
         Map<String,String> generatedNames = new HashMap<String, String>();
 
