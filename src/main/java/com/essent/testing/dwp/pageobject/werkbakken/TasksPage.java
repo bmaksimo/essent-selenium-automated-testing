37e60ddc168643622cc4d37759e0b75c0660f3a9
mkolarov <mi.kolarov@levi9.com>
2019-03-18 13:15:37 +0100

    refactored TaskPage class to reflect PO principle

--- src/main/java/com/essent/testing/dwp/pageobject/werkbakken/TasksPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/werkbakken/TasksPage.java
@@ -2,20 +2,39 @@ package com.essent.testing.dwp.pageobject.werkbakken;
 
 import com.essent.testing.dwp.pageobject.impl.Component;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class TasksPage extends Component {
-    public String getTaskId() {
-        final String taskId;
-        taskId = seleniumDriver.findElementWhenVisible(By.xpath("(//h6)[2]")).getText();
-        return taskId;
-    }
 
-    public void inputResolution(String text) {
-        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.id("task-resolution-c-field")), text);
+    private static final String taskIDXPath = "(//h6)[2]";  
+    private static final String inputResolutionId = "task-resolution-c-field";
+    private static final String taskNumberId = "task-number-c-default-value-field";
+
+    public WebElement findElementbyX(String path){
+	WebElement element = seleniumDriver.findElementWhenVisible(By.xpath(path));
+	return element;
+    }
+    
+    public WebElement findElementbyId(String id){
+  	WebElement element = seleniumDriver.findElementWhenVisible(By.id(id));
+  	return element;
+      }
+
+    public String getTaskId() {
+	return findElementbyX(taskIDXPath).getText();
+    }
+    
+    public void waitAndInput(WebElement element, String text) {
+	seleniumDriver.waitAndSendKeys(element, text);
+    }
+    
+     
+    public void inputResolution(String text) {	
+	waitAndInput(findElementbyId(inputResolutionId), text);
     }
 
     public void findTaskId(String taskId) {
-        seleniumDriver.waitForRequestsToFinish();
-        seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.id("task-number-c-default-value-field")), taskId);
+	seleniumDriver.waitForRequestsToFinish();
+	waitAndInput(findElementbyId(taskNumberId), taskId);
     }
 }
