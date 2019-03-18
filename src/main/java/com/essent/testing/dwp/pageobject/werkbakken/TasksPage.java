b3de02ea08abe9c9899bce1704bf193957dea099
mkolarov <mi.kolarov@levi9.com>
2019-03-18 13:22:31 +0100

    Updated TastPage

--- src/main/java/com/essent/testing/dwp/pageobject/werkbakken/TasksPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/werkbakken/TasksPage.java
@@ -1,34 +1,19 @@
 package com.essent.testing.dwp.pageobject.werkbakken;
 
-import com.essent.testing.dwp.pageobject.impl.Component;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import com.essent.testing.dwp.pageobject.impl.page.BaseObjectPage;
 
-public class TasksPage extends Component {
+public class TasksPage extends BaseObjectPage {
 
     private static final String taskIDXPath = "(//h6)[2]";  
     private static final String inputResolutionId = "task-resolution-c-field";
     private static final String taskNumberId = "task-number-c-default-value-field";
-
-    public WebElement findElementbyX(String path){
-	WebElement element = seleniumDriver.findElementWhenVisible(By.xpath(path));
-	return element;
-    }
+           
     
-    public WebElement findElementbyId(String id){
-  	WebElement element = seleniumDriver.findElementWhenVisible(By.id(id));
-  	return element;
-      }
-
     public String getTaskId() {
 	return findElementbyX(taskIDXPath).getText();
     }
-    
-    public void waitAndInput(WebElement element, String text) {
-	seleniumDriver.waitAndSendKeys(element, text);
-    }
-    
-     
+   
+   
     public void inputResolution(String text) {	
 	waitAndInput(findElementbyId(inputResolutionId), text);
     }
