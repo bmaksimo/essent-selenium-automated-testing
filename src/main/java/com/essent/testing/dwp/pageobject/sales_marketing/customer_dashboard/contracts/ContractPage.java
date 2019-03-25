097e87891b0aca89c97f8654d88b97d2f073ddda
nikola_g <n.gajic@levi9.com>
2019-03-25 11:06:16 +0100

    added final

--- src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/contracts/ContractPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/contracts/ContractPage.java
@@ -25,7 +25,7 @@ public class ContractPage extends Component {
 
 
     public String date = simpleDateFormat.format(new Date());
-    private static String xpath = "//div[@class = 'non-editable-editor']";
+    private static final String xpath = "//div[@class = 'non-editable-editor']";
 
     public void startDateIsToday() {
         seleniumDriver.waitAndSendKeys(startData(),"date");
