1ffeb694f443b29827f85f1b1d789a742908e2e2
nikola_g <n.gajic@levi9.com>
2019-03-26 15:51:39 +0100

    removed commented code

--- src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/documents/DocumentsPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/documents/DocumentsPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.By;
 public class DocumentsPage extends Component {
 
     public String documentText(){
-//        return findElementWhenVisible(By.xpath("(//list-simple-two-liner-cell//span[1])[4]")).getText();
         return findElementWhenVisible(By.xpath("//span[contains(text(), 'customer-signature')]")).getText();
 
     }
