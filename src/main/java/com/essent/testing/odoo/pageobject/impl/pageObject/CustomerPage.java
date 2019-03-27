9da488d3f9d70f271d7cf65ba224c47c6fc9fa97
nikola_g <n.gajic@levi9.com>
2019-03-27 13:57:41 +0100

    changed xpath to use query

--- src/main/java/com/essent/testing/odoo/pageobject/impl/pageObject/CustomerPage.java
+++ src/main/java/com/essent/testing/odoo/pageobject/impl/pageObject/CustomerPage.java
@@ -7,10 +7,12 @@ public class CustomerPage extends Component {
 
     private static final String labelPaymentMethod = "//tr[7]/td[2]/span";
     private static final String labelBankAccount = "//td[@data-field=\"acc_number\"]";
+    private static final String nameTab = "name_tab";
+    private static final String tabMenuXpath = "//li[@class='ui-state-default ui-corner-top']/a[contains(text(),'"+nameTab+"')] ";
 
     public void clickOnTabMenu(String tab){
         awaitOdooRequestToFinish(20);
-        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//li[@class='ui-state-default ui-corner-top']/a[contains(text(),'"+tab+"')] ")));
+        createQuery(tabMenuXpath, nameTab, tab);
 
     }
 
