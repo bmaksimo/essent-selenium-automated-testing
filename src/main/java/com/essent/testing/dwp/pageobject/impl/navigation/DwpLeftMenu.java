227fb6e1909fe79dd2c900790a01051e264daa46
mkolarov <mi.kolarov@levi9.com>
2019-03-28 11:04:41 +0100

    added changes to page class

--- src/main/java/com/essent/testing/dwp/pageobject/impl/navigation/DwpLeftMenu.java
+++ src/main/java/com/essent/testing/dwp/pageobject/impl/navigation/DwpLeftMenu.java
@@ -6,15 +6,15 @@ import org.openqa.selenium.WebElement;
 
 public class DwpLeftMenu extends Component {
 
-    private static final String NAME_KEY="name_key";
-    private static final String LEFT_MENU_XPATH = "//main-menu-link[@name='${" + NAME_KEY + "}']//a"; 
-    
-    private WebElement getLeftElement(String nameValue)  {
-	String xpath = createQuery(LEFT_MENU_XPATH, NAME_KEY, nameValue); 
+    private static final String NAME_KEY = "name_key";
+    private static final String LEFT_MENU_XPATH = "//main-menu-link[@name='${" + NAME_KEY + "}']//a";
+
+    private WebElement getLeftElement(String nameValue) {
+	String xpath = createQuery(LEFT_MENU_XPATH, NAME_KEY, nameValue);
 	return seleniumDriver.findElementWhenVisible(By.xpath(xpath));
     }
 
     public void clickOnLeftElement(String element) {
-        seleniumDriver.waitAndClick(getLeftElement(element));
+	seleniumDriver.waitAndClick(getLeftElement(element));
     }
 }
