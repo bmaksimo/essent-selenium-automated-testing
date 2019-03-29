1b53fb3244f0dff8cd5b73747e6d8bbf1dd8c86d
nikola_g <n.gajic@levi9.com>
2019-03-29 15:34:09 +0100

    also this one

--- src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/details/DetailsPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/details/DetailsPage.java
@@ -6,13 +6,13 @@ import org.openqa.selenium.By;
 
 public class DetailsPage extends Component {
 
-    private static final String numberBillingCustomer = "//list[@list-key='BillingCustomerOnaccount']//tr[@class='list__row']";
+    private static final String NUMBER_BILLING_CUSTOMER = "//list[@list-key='BillingCustomerOnaccount']//tr[@class='list__row']";
     public void findIban(String iban) {
         seleniumDriver.waitForRequestsToFinish();
         Assert.assertTrue(seleniumDriver.findElementWhenVisible(By.xpath("//span[.='" + iban + "']")).isDisplayed());
     }
 
     public int getNumberOfBillingCustomers(){
-        return seleniumDriver.findElements(By.xpath(numberBillingCustomer)).size();
+        return seleniumDriver.findElements(By.xpath(NUMBER_BILLING_CUSTOMER)).size();
     }
 }
