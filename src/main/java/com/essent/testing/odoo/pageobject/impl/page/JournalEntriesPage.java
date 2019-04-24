ffb69aa13683db1fc2c2b258b5c001fe1d0e19d9
sonja <s.novakovic@levi9.com>
2019-04-24 15:56:31 +0200

    NSTA-466 removed unused methods

--- src/main/java/com/essent/testing/odoo/pageobject/impl/page/JournalEntriesPage.java
+++ src/main/java/com/essent/testing/odoo/pageobject/impl/page/JournalEntriesPage.java
@@ -20,10 +20,6 @@ public class JournalEntriesPage extends Component {
         seleniumDriver.findElementWhenVisible(By.xpath("//button[@class='oe_button oe_list_add oe_highlight']")).click();
     }
 
-    public void clickOnDropDownButtonJournal(){
-        seleniumDriver.findElementWhenVisible(By.xpath("//span[@class='oe_m2o_drop_down_button']"));
-    }
-
     public void chooseDiverseDagboekKlanten(String journal) {
         seleniumDriver.findElementWhenVisible(By.xpath("(//input[@class='ui-autocomplete-input'])[1]")).sendKeys(journal);
         seleniumDriver.findElementWhenVisible(By.xpath("//a[contains(text(),'Diverse dagboek klanten (EUR)')]")).click();
@@ -63,31 +59,6 @@ public class JournalEntriesPage extends Component {
         WebElement partnerLink = seleniumDriver.findElementWhenVisible(By.xpath("//a[contains(text(),'B2B_UP')]"));
         seleniumDriver.moveToElementAndClick(partnerLink);
     }
-
-    public WebElement findAccountElement(){
-        return seleniumDriver.findElementWhenVisible(By.xpath("//span[@data-fieldname='account_id']/div/input"));
-    }
-
-    public void setAccount(String account){
-
-        findAccountElement().click();
-        findAccountElement().clear();
-        findAccountElement().sendKeys(account);
-        WebElement accountLink = seleniumDriver.findElementWhenVisible(By.xpath("//a[contains(text(),'580100 B2C-B2B  OVERBOEKINGEN')]"));
-        seleniumDriver.moveToElementAndClick(accountLink);
-    }
-
-
-    public WebElement findDebitElement(){
-        return seleniumDriver.findElementWhenVisible(By.xpath("//span[@data-fieldname='debit']/input"));
-    }
-
-    public void setDebit(String debit){
-        findDebitElement().click();
-        findDebitElement().clear();
-        findDebitElement().sendKeys(debit);
-    }
-
 
     public WebElement findCreditElement(){
         return seleniumDriver.findElementWhenVisible(By.xpath("//span[@data-fieldname='credit']/input"));
