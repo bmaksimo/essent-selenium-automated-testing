7ad5be802496c5f0ae9b69d6c781e7051058e57d
Dmitry Chebayewski <dmitry.chebayewski@billinghouse.nl>
2019-05-27 16:09:00 +0200

    BSTA-592, rolling back experimental code

--- src/main/java/com/essent/testing/dwp/pageobject/impl/quote/QuoteCreationGuidedStep.java
+++ src/main/java/com/essent/testing/dwp/pageobject/impl/quote/QuoteCreationGuidedStep.java
@@ -6,61 +6,57 @@ import com.essent.testing.dwp.pageobject.quote.GuidedStep;
 import cucumber.runtime.CucumberException;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.awaitility.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
+import static com.billinghouse.test_automation.javascript.testrunner.JsTestRegistry.JS_TR_IS_NEXT_BUTTON_ENABLED;
 import static com.essent.testing.dwp.pageobject.selector.CommonSelectors.NEXT_BUTTON;
 import static com.essent.testing.dwp.pageobject.selector.CommonSelectors.VIEW;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.given;
-import static org.awaitility.Duration.FIVE_HUNDRED_MILLISECONDS;
-import static org.awaitility.Duration.TWO_SECONDS;
 
 public abstract class QuoteCreationGuidedStep extends Component implements GuidedStep, Form {
 
-    private static By STANDARD_UI_VIEW = By.xpath(VIEW.getQuery());
+  private static By STANDARD_UI_VIEW = By.xpath(VIEW.getQuery());
+  private static By MANDATORY_INPUT_EXCLAMATION_CSS = By.cssSelector(".is-error");
 
-    public QuoteCreationGuidedStep() {
-        super(STANDARD_UI_VIEW);
-    }
+  public QuoteCreationGuidedStep() {
+    super(STANDARD_UI_VIEW);
+  }
 
-    @Override
-    public void next() {
-        seleniumDriver.waitForRequestsToFinish();
-        logMandatoryInputStatus();
-        logger().debug("Searching element by " + NEXT_BUTTON.getQuery());
-        given().await()
-            .ignoreExceptions()
-            .pollInterval(FIVE_HUNDRED_MILLISECONDS)
-            .pollDelay(TWO_SECONDS)
-            .atMost(new Duration(60, SECONDS)).until(this::isNextButtonEnabled);
-        WebElement nextButton = findElementWhenClickable(By.cssSelector(NEXT_BUTTON.getQuery()));
+  @Override
+  public void next() {
+    seleniumDriver.waitForRequestsToFinish();
+    logger().debug("Guided step to be confirmed");
+    logMandatoryInputStatus();
+    Optional<WebElement> nextButtonOptional = Optional.of(findElementWhenClickable(By.cssSelector(NEXT_BUTTON.getQuery())));
+    if (nextButtonOptional.isPresent()) {
+      WebElement nextButton = nextButtonOptional.get();
+      logger().debug("Found  element: " + nextButton.getTagName());
+      logger().debug("CLICK ");
         logger().info("- RESULT: Confirm guidance step, confirmation button attribute value: Next[disabled] = " + nextButton.getAttribute("disabled"));
-        if(logger().isDebugEnabled())
-        {
-            seleniumDriver.takeScreenshot("guidance-confirm-");
-        }
-        if (nextButton != null && nextButton.isEnabled()) {
-            logger().debug("Found  element: " + nextButton.getTagName());
-            logger().debug("CLICK ");
-            seleniumDriver.waitForRequestsToFinish();
-            nextButton.click();
-        } else {
-            if(logger().isDebugEnabled()) {
-                seleniumDriver.takeScreenshot("guidance-confirm-failure");
-            }
-            throw new CucumberException("Element not found by selector " + NEXT_BUTTON.getQuery());
-        }
         seleniumDriver.waitForRequestsToFinish();
+      nextButton.click();
+    } else {
+        if(logger().isDebugEnabled()) {
+            seleniumDriver.takeScreenshot("guidance-confirm-failure");
+        }
+        throw new CucumberException("Element not found by selector " + NEXT_BUTTON.getQuery());
     }
+    seleniumDriver.waitForRequestsToFinish();
+  }
+
+  public Boolean isNextButtonEnabled() {
+    Map options = new HashMap<>();
+    Map result = seleniumDriver.executeJavascriptMethod(JS_TR_IS_NEXT_BUTTON_ENABLED, options);
+    return BooleanUtils.toBoolean((String) result.get("enabled"));
+  }
 
     private void logMandatoryInputStatus() {
-        List<WebElement> elements = seleniumDriver.findElements(By.cssSelector(".is-error"),
+        List<WebElement> elements = seleniumDriver.findElements(MANDATORY_INPUT_EXCLAMATION_CSS,
             java.time.Duration.ofSeconds(1),
             java.time.Duration.ofMillis(200));
         String location = elements.stream().map(WebElement::getText).reduce("", (partialString, element) -> partialString + (" " + element + System.lineSeparator()));
@@ -69,11 +65,4 @@ public abstract class QuoteCreationGuidedStep extends Component implements Guide
         }
     }
 
-    public Boolean isNextButtonEnabled() {
-        Map options = new HashMap<>();
-        Map result = seleniumDriver.executeJavascriptMethod("TrIsNextButtonEnabled", options);
-        String buttonStatus = (String) result.get("enabled");
-        logger().info("- RESULT: Confirm guidance step, confirmation button is enabled: " + buttonStatus);
-        return BooleanUtils.toBoolean(buttonStatus);
-    }
 }
