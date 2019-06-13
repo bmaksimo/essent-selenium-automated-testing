e03d7830a786f658942dc1f3e62f5be38dc707bf
nikola_g <n.gajic@levi9.com>
2019-06-13 16:37:27 +0200

    fixed regex

--- src/main/java/stepdefinitions/dwp/page_object/MoveOldAddressSteps.java
+++ src/main/java/stepdefinitions/dwp/page_object/MoveOldAddressSteps.java
@@ -34,7 +34,7 @@ public class MoveOldAddressSteps extends DwpScenario {
         cmoa.chooseMoveDate(inputValue);
     }
 
-    @And("^Get meter reading plus \"(\\d+)\"kwl from \"(\\d+)\"$")
+    @And("^Get meter reading plus \"([^\"]*)\" kwl from \"([^\"]*)\"$")
     public void getMeterReadingPlusKwl(int num, String rate){
         CreateMoveOAPage cmoa = new CreateMoveOAPage();
         String oldMeterReading = cmoa.getPreviousMeterReading(rate);
