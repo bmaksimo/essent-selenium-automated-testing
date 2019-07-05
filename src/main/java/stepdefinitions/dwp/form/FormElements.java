fc6a3f67e58a1d046c69fd592443dda80ddf05bc
nikola_g <n.gajic@levi9.com>
2019-07-05 12:36:03 +0200

    increased time for waiting header since page is slow opening

--- src/main/java/stepdefinitions/dwp/form/FormElements.java
+++ src/main/java/stepdefinitions/dwp/form/FormElements.java
@@ -51,7 +51,7 @@ public class FormElements extends DwpScenario {
         given().await()
             .pollInterval(FIVE_HUNDRED_MILLISECONDS)
             .pollDelay(ONE_SECOND)
-            .atMost(new Duration(30, SECONDS)).until(() -> new CheckFormHeader().test(formHeader));
+            .atMost(new Duration(90, SECONDS)).until(() -> new CheckFormHeader().test(formHeader));
     }
 
     @And("^\"([^\"]*)\" field value is \"([^\"]*)\"$")
