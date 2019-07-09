fe0af72cb03d816c26a30b9279285f8dce96e86f
nikola_g <n.gajic@levi9.com>
2019-07-09 16:19:48 +0200

    changed wording

--- src/main/java/stepdefinitions/dwp/top_actions/TopActions.java
+++ src/main/java/stepdefinitions/dwp/top_actions/TopActions.java
@@ -82,7 +82,7 @@ public class TopActions extends NavigationElements {
             success, is(true));
     }
 
-    @And("Possible Alert window is handled$")
+    @And("Intermittent Alert window is handled$")
     public void handleAlert() {
         boolean actualAlert = isAlertPresent();
         if (actualAlert)
