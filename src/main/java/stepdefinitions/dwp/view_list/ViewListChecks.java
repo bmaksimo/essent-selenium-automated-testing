e0098fd73a23656c309dfbd72c929754a47d0c23
Marcelo Cure <marcelocure@gmail.com>
2019-07-30 14:48:49 +0200

    Increasing times and adding fluentwaiting for other table checking steps

--- src/main/java/stepdefinitions/dwp/view_list/ViewListChecks.java
+++ src/main/java/stepdefinitions/dwp/view_list/ViewListChecks.java
@@ -202,7 +202,7 @@ public class ViewListChecks extends NavigationElements {
         String arrow = parameterProvider.getValueOrParameterAsString("parameter:navigation");
         String dashboardMenu = parameterProvider.getValueOrParameterAsString("parameter:dashboard-menu");
 
-        FluentWait<ViewListTestObject> waiter = waiter(new ViewListTestObject(), waitingTime, 20);
+        FluentWait<ViewListTestObject> waiter = waiter(new ViewListTestObject(), waitingTime, 60);
         waiter.withMessage(String.format(
             "Table does not contain cell value \"%s\" at column \"%s\" within \"%s\" seconds.",
             match, columnName, waitingTime));
