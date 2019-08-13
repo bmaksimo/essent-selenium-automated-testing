7b2837a61365aac11bec5a0e55372a7c20f55dff
nikola_g <n.gajic@levi9.com>
2019-08-13 11:34:48 +0200

    fix for failing step on market messages checks

--- src/main/java/stepdefinitions/dwp/page_object/MarketBerichtenSteps.java
+++ src/main/java/stepdefinitions/dwp/page_object/MarketBerichtenSteps.java
@@ -123,15 +123,16 @@ public class MarketBerichtenSteps extends DwpScenario {
         String modul = info.get(1).get(1);
         String date = info.get(1).get(2);
         MarktBerichtenPage mp = new MarktBerichtenPage();
-        if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("1"))){
+        String module = "UPDATE BUSINESS MASTER DATA";
+        if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("1")) && !(mp.getModulFromMarketbericht("2").equals(module)) ){
             Assert.assertEquals(modul, mp.getModulFromMarketbericht("2"));
             Assert.assertEquals(mp.getMarketberichtEndDateElement("1"), toDwpEndDate(parameterProvider.getValueOrParameterAsString(date)));
         } else {
-            if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("3"))) {
+            if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("3")) && !(mp.getModulFromMarketbericht("4").equals(module)) ) {
                 Assert.assertEquals(modul, mp.getModulFromMarketbericht("4"));
                 Assert.assertEquals(mp.getMarketberichtEndDateElement("5"), toDwpEndDate(parameterProvider.getValueOrParameterAsString(date)));
             } else {
-                if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("5"))) {
+                if (ean.equalsIgnoreCase(mp.getEanFromMarketbericht("5")) && !(mp.getModulFromMarketbericht("6").equals(module)) ){
                     Assert.assertEquals(modul, mp.getModulFromMarketbericht("6"));
                     Assert.assertEquals(mp.getMarketberichtEndDateElement("9"), toDwpEndDate(parameterProvider.getValueOrParameterAsString(date)));
                 }
