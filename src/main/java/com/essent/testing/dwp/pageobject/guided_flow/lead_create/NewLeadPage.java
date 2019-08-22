ae7e17b1c34873b6e21fe250cd0295bcd44d043b
Fernando Mano <fernando.mano@billinghouse.nl>
2019-08-22 16:47:09 +0200

    [FIX] TESTAUTO-140: Fix NUAT-553: Create and convert lead - remove comments and organize imports

--- src/main/java/com/essent/testing/dwp/pageobject/guided_flow/lead_create/NewLeadPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/guided_flow/lead_create/NewLeadPage.java
@@ -1,30 +1,17 @@
 package com.essent.testing.dwp.pageobject.guided_flow.lead_create;
 
-
-import com.essent.automation.util.Sleeper;
-import com.essent.testing.dwp.pageobject.impl.elements.ToggleImpl;
 import com.essent.testing.dwp.pageobject.impl.page.BaseObjectPage;
 import org.openqa.selenium.By;
-
-
 import java.util.List;
 import java.util.Map;
 
 public class NewLeadPage extends BaseObjectPage {
 
     private final static String COMPANYNAME="company-name-c-field";
-//    private final static String COMPANYNAMEBUTTON=".//*[@id='company_name_c']/div/autocomplete/ul";
-//    private final static String COMPANYNAMEBUTTON=".//*[@id='company_name_c']/div/autocomplete/ul/li[4]";
     private final static String FIRSTNAME="first-name-field";
     private final static String LASTNAME="last-name-field";
 
-  //TODO Remove locale-specific hardcode.
-  // The project must support official Belgian languages.
-  // Locale-specific elements of web element locators must be parameterized.
-  // This is basic rule!
-
     public void createLead(List<Map<String,String>> table) {
-        ToggleImpl tg = new ToggleImpl();
         String companyNumber = null;
         String contactPersonFirstName = null;
         String contactPersonLastName = null;
@@ -37,13 +24,11 @@ public class NewLeadPage extends BaseObjectPage {
 
         fillInCompanyName(companyNumber);
         seleniumDriver.waitForRequestsToFinish();
-//        Sleeper.sleepTightInSeconds(2);
         fillInContactPerson(contactPersonFirstName, contactPersonLastName);
     }
 
     private void fillInCompanyName(String companyNumber) {
         seleniumDriver.waitAndSendKeys(findElementWhenVisible(By.id(COMPANYNAME)), companyNumber);
-//        seleniumDriver.waitAndClick(findElementWhenVisible(By.xpath(COMPANYNAMEBUTTON)));
         seleniumDriver.waitForRequestsToFinish();
     }
 
