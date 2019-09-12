828d42d2d5dee8fb130f4d837c32d50735a79755
Marcelo Cure <marcelocure@gmail.com>
2019-09-12 17:47:26 +0200

    Remove log

--- src/main/java/stepdefinitions/dwp/contracts/b2c/ContractB2CScenario.java
+++ src/main/java/stepdefinitions/dwp/contracts/b2c/ContractB2CScenario.java
@@ -38,8 +38,6 @@ public class ContractB2CScenario extends RegisteredScenario {
 			logger.error("Something went wrong with creation of B2C contract");
 		}
 
-		logger.info("ACCOUNT NUMBER: " + this.getAccountNumber());
-
         String accountNumber = this.getAccountNumber();
         parameterProvider.put("accountNumber", accountNumber);
 		return accountNumber;
