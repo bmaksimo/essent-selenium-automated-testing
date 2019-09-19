c3c91b54860fcdf1badd108fc696d6e6ccab31a7
nikola_g <n.gajic@levi9.com>
2019-09-19 16:19:43 +0200

    removed waitAndClick

--- src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/sales/SalesPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/sales/SalesPage.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 public class SalesPage extends Component {
     public void inputText(String text) {
         seleniumDriver.waitForRequestsToFinish();
-        seleniumDriver.waitAndClick(seleniumDriver.findElementWhenVisible(By.xpath("//text-angular[@id='description-field']/div[2]/div[3]")));
         seleniumDriver.waitAndSendKeys(seleniumDriver.findElementWhenVisible(By.xpath("//text-angular[@id='description-field']/div[2]/div[3]")), text);
     }
 }
