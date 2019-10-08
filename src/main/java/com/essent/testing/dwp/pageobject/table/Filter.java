fd3af23543297eb9b0f5a9bc02410c0675bd32fa
Marcelo Cure <marcelocure@gmail.com>
2019-10-08 23:37:33 +0200

    class content :|

--- src/main/java/com/essent/testing/dwp/pageobject/table/Filter.java
+++ src/main/java/com/essent/testing/dwp/pageobject/table/Filter.java
@@ -1,4 +1,19 @@
 package com.essent.testing.dwp.pageobject.table;
 
 public class Filter {
+    private String columnName;
+    private String columnValue;
+
+    public Filter(String columnName, String columnValue) {
+        this.columnName = columnName;
+        this.columnValue = columnValue;
+    }
+
+    public String getColumnName() {
+        return columnName;
+    }
+
+    public String getColumnValue() {
+        return columnValue;
+    }
 }
