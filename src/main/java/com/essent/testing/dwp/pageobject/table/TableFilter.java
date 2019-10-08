bec79896745f7ec1c0f8b900d2be3d286dc81aa9
Marcelo Cure <marcelocure@gmail.com>
2019-10-08 22:09:16 +0200

    Adding TableFilter to filter generic tables

---
+++ src/main/java/com/essent/testing/dwp/pageobject/table/TableFilter.java
@@ -0,0 +1,59 @@
+package com.essent.testing.dwp.pageobject.table;
+
+import com.essent.testing.dwp.pageobject.impl.Component;
+import cucumber.runtime.CucumberException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TableFilter extends Component {
+
+    private List<String> headers;
+    private List<WebElement> rows;
+    private List<WebElement> selectedRow;
+
+    public TableFilter getTable(String tableName) {
+        seleniumDriver.waitForRequestsToFinish();
+        this.headers = getHeaders(tableName);
+        this.rows = seleniumDriver.findElements(By.xpath("//list[@list-key='" + tableName + "']//tbody[@id='rows']/tr"));
+        return this;
+    }
+
+    public TableFilter filterBy(String columnName, String columnValue) {
+        for (WebElement row : this.rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            if (isExpectedColumnValue(columnName, columnValue, cells)) {
+                selectedRow = cells;
+                return this;
+            }
+        }
+        throw new CucumberException(columnValue + " was not found on column " + columnName + " on any row of the table");
+    }
+
+    private boolean isExpectedColumnValue(String columnName, String columnValue, List<WebElement> cells) {
+        try {
+            int columnIndex = headers.indexOf(columnName);
+            return cells.get(columnIndex).getText().equals(columnValue);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public List<WebElement> get() {
+        return this.selectedRow;
+    }
+
+    public void plusActionIsClicked() {
+        //TODO implement this method
+    }
+
+    private List<String> getHeaders(String table) {
+        return seleniumDriver
+            .findElements(By.xpath("//list[@list-key='" + table + "']//thead/tr/th"))
+            .stream()
+            .map(WebElement::getText)
+            .collect(Collectors.toList());
+    }
+}
