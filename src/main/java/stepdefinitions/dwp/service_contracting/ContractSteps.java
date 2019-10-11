78165189b59deb286c5ed3dc8f4f334f3f33a9e8
Marcelo Cure <marcelocure@gmail.com>
2019-10-11 19:59:00 +0200

    Removing chars commited by mistake

--- src/main/java/stepdefinitions/dwp/service_contracting/ContractSteps.java
+++ src/main/java/stepdefinitions/dwp/service_contracting/ContractSteps.java
@@ -247,6 +247,6 @@ public class ContractSteps extends DwpScenario {
     @And("^Message \"([^\"]*)\" is shown$")
     public void communicationPreferencesUpdatedSuccessMessageIsShown(String message) {
         String elementMessage = new DetailsPage().getCommunicationPreferencesUpdatedMessage();
-gd         Assert.assertTrue("Communication preference message is not as expected", message.equalsIgnoreCase(elementMessage));
+        Assert.assertTrue("Communication preference message is not as expected", message.equalsIgnoreCase(elementMessage));
     }
 }
