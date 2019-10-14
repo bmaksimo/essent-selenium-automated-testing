ecb6e5b82d37c6890d17580f870dddd50d50853c
Fernando Mano <fernando.mano@billinghouse.nl>
2019-10-14 19:27:02 +0200

    [TASK] TESTAUTO-302: Remove TrCheckModalDialog and usages; remove unused imports

--- src/main/java/stepdefinitions/dwp/modal/confirm/ConfirmationSteps.java
+++ src/main/java/stepdefinitions/dwp/modal/confirm/ConfirmationSteps.java
@@ -12,9 +12,6 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import stepdefinitions.dwp.navigation.NavigationElements;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
