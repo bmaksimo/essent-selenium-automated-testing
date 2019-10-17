2873c0c2e1a2650f572435ded0c36326a92b0f37
nikola_g <n.gajic@levi9.com>
2019-10-17 10:58:06 +0200

    added waiting time

--- src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/contracts/ContractPage.java
+++ src/main/java/com/essent/testing/dwp/pageobject/sales_marketing/customer_dashboard/contracts/ContractPage.java
@@ -463,19 +463,20 @@ public class ContractPage extends Component {
 
     public float sumRates(String typeRate) {
         seleniumDriver.waitForRequestsToFinish();
+        Sleeper.sleepTightInSeconds(5);
         String typeQuoteRate = "";
 
         if (typeRate.contains("High")) {
             typeQuoteRate = HIGH_RATES_QUOTE;
-
         }
         else if (typeRate.contains("Low")) {
             typeQuoteRate = LOW_RATES_QUOTE;
         }
 
         List<WebElement> rates = seleniumDriver.findElements(By.xpath(typeQuoteRate));
-        float sumRate=0.000f;
+        float sumRate=0.000F;
         for (WebElement matchValue : rates ) {
+            Sleeper.sleepTightInSeconds(2);
             String [] str = matchValue.getText().split(",");
             String finalStr = str[0] + "." + str[1];
             sumRate += Float.parseFloat(finalStr);
