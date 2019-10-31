ee1f4628f71008e989bc772bc7825c80460a28e0
Marcelo Cure <marcelocure@gmail.com>
2019-10-31 15:19:45 +0100

    Adding back method

--- src/main/java/stepdefinitions/dwp/plus/PlusActions.java
+++ src/main/java/stepdefinitions/dwp/plus/PlusActions.java
@@ -1,8 +1,11 @@
 package stepdefinitions.dwp.plus;
 
+import com.essent.automation.util.Sleeper;
 import cucumber.api.Scenario;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.runtime.CucumberException;
 import stepdefinitions.dwp.navigation.NavigationElements;
 
 public class PlusActions extends NavigationElements {
@@ -10,6 +13,17 @@ public class PlusActions extends NavigationElements {
     @Before("@DWP or @CORE or @E2E or @REGRESSION")
     public void setupTest(Scenario scenario) throws Throwable {
         registerActiveScenario(scenario);
+    }
+
+    public void checkPlusMenu(String path) throws CucumberException {
+        try {
+            seleniumDriver.waitForRequestsToFinish();
+            clickTopAction("Plus Menu");
+            clickPlusAction(path);
+            parameterProvider.put("plus-menu-item", path);
+        } catch (Throwable t) {
+          throw  new CucumberException(t);
+        }
     }
 
     @Override
