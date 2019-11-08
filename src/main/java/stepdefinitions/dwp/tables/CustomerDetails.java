2526350e485fd04b589e874d5f67fa59ebfcb356
Fernando Mano <fernando.mano@billinghouse.nl>
2019-11-08 18:36:18 +0100

    [FIX] TESTAUTO-378: switch from UI to API contract creation; other minor improvements; organize imports

--- src/main/java/stepdefinitions/dwp/tables/CustomerDetails.java
+++ src/main/java/stepdefinitions/dwp/tables/CustomerDetails.java
@@ -1,7 +1,6 @@
 package stepdefinitions.dwp.tables;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.lang.reflect.Field;
