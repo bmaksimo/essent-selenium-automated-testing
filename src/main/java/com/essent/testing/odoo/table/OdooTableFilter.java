5c5ddefb1dd79244324b29ba34a4fb0436a05ceb
Marcelo Cure <marcelocure@gmail.com>
2019-11-15 17:14:20 +0100

    Removing comment

--- src/main/java/com/essent/testing/odoo/table/OdooTableFilter.java
+++ src/main/java/com/essent/testing/odoo/table/OdooTableFilter.java
@@ -5,7 +5,7 @@ import com.essent.testing.table.TableFilterBase;
 
 public class OdooTableFilter extends TableFilterBase {
 
-    private static final String TABLE_PATH_BASE = "//div[@data-view-type='list']//table[@class='oe_list_content']";//"//table[@class='oe_list_content']";
+    private static final String TABLE_PATH_BASE = "//div[@data-view-type='list']//table[@class='oe_list_content']";
     private static final String TABLE_ROWS_BASE = "//tbody/tr";
 
     public OdooTableFilter(String contextParameters, SeleniumDriver seleniumDriver) {
