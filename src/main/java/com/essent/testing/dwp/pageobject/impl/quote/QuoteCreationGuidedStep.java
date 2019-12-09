2b5971a4bbaf9b8d42c364333aeedcd0a540e94c
Fernando Mano <fernando.mano@billinghouse.nl>
2019-12-09 19:58:13 +0100

    [FIX] Improve next button with retry mechanism

--- src/main/java/com/essent/testing/dwp/pageobject/impl/quote/QuoteCreationGuidedStep.java
+++ src/main/java/com/essent/testing/dwp/pageobject/impl/quote/QuoteCreationGuidedStep.java
@@ -4,22 +4,15 @@ import com.essent.automation.util.Sleeper;
 import com.essent.testing.dwp.pageobject.Form;
 import com.essent.testing.dwp.pageobject.impl.Component;
 import org.apache.commons.lang3.StringUtils;
-import org.awaitility.Duration;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-import static com.essent.testing.dwp.pageobject.selector.CommonSelectors.NEXT_BUTTON;
 import static com.essent.testing.dwp.pageobject.selector.CommonSelectors.VIEW;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.given;
-import static org.awaitility.Duration.FIVE_HUNDRED_MILLISECONDS;
-import static org.awaitility.Duration.ONE_HUNDRED_MILLISECONDS;
 
 public abstract class QuoteCreationGuidedStep extends Component implements Form {
 
     private static By STANDARD_UI_VIEW = By.xpath(VIEW.getQuery());
-    private static By NEXT_BUTTON_CSS_SELECTOR = By.cssSelector(NEXT_BUTTON.getQuery());
 
     public QuoteCreationGuidedStep() {
         super(STANDARD_UI_VIEW);
@@ -39,7 +32,6 @@ public abstract class QuoteCreationGuidedStep extends Component implements Form 
         logger().debug("Guided step to be confirmed");
         validateForm(scenarioInfo);
         closeGuidanceModalIfPresent();
-//        WebElement nextButton = seleniumDriver.findElement(NEXT_BUTTON_CSS_SELECTOR);
         WebElement nextButton = null;
         try {
             nextButton = findElementWithRetries(By.id("primaryButton"), 50);
@@ -58,18 +50,6 @@ public abstract class QuoteCreationGuidedStep extends Component implements Form 
         } catch (Exception e) {
             Assert.fail("NEXT button was not found or is disabled.");
         }
-//        logger().debug("Found  element: " + nextButton.getTagName());
-//        logger().debug("- RESULT: Confirm guidance step, confirmation button attribute value: Next[disabled] = " + nextButton.getAttribute("disabled"));
-//        given().await()
-//            .ignoreExceptions()
-//            .pollInterval(FIVE_HUNDRED_MILLISECONDS)
-//            .pollDelay(ONE_HUNDRED_MILLISECONDS)
-//            .atMost(new Duration(60, SECONDS)).until(this::isNextButtonEnabled);
-//        nextButton.click();
         seleniumDriver.waitForRequestsToFinish();
-    }
-
-    public Boolean isNextButtonEnabled() {
-        return seleniumDriver.findElementOptional(NEXT_BUTTON_CSS_SELECTOR).isPresent();
     }
 }
